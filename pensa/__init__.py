bb946218cc3db7a59e2eb4e5168bd2c3a2947a01
martinvoegele <martinvoegele1989@gmail.com>
2020-10-07 05:50:46 -0700

    structured as package

---
+++ pensa/__init__.py
@@ -0,0 +1,15 @@
+import numpy as np
+import scipy as sp
+import scipy.stats
+import mdshare
+import pyemma
+from pyemma.util.contexts import settings
+import MDAnalysis as mda
+import matplotlib.pyplot as plt
+
+from .analysis import *
+from .clusters import *
+from .comparison import *  
+from .pca import *
+from .preprocessing import *
+
