bd19cfb093d4c3f5653b53b8b8d97c9557a4334f
Martin Voegele <martinvoegele1989@gmail.com>
2020-10-09 05:34:07 -0700

    vmd vis script

---
+++ scripts/residue_visualization.tcl
@@ -0,0 +1,14 @@
+display projection Orthographic
+axes location Off
+
+mol modstyle 0 0 NewCartoon 0.300000 10.000000 4.100000 0
+mol modcolor 0 0 Beta
+mol modmaterial 0 0 AOChalky
+
+color scale method BWR
+color scale midpoint 0.3
+color scale min -0.25
+color scale max 1.0
+color Display Background white
+menu color on
+
