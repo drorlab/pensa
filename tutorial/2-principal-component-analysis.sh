596ad0c61a4c6b31423762181632bc2bdaa9bfb7
martinvoegele <martinvoegele1989@gmail.com>
2020-10-13 05:29:13 -0700

    args in tutorial 2/3

--- tutorial/2-principal-component-analysis.sh
+++ tutorial/2-principal-component-analysis.sh
@@ -1,4 +1,15 @@
 mkdir -p pca
 
-python ~/pensa/scripts/calculate_combined_principal_components.py
+python ~/pensa/scripts/calculate_combined_principal_components.py \
+	--ref_file_a 'traj/rhodopsin_arrbound_receptor.gro' \
+	--trj_file_a 'traj/rhodopsin_arrbound_receptor.xtc' \
+	--ref_file_b 'traj/rhodopsin_gibound_receptor.gro' \
+	--trj_file_b 'traj/rhodopsin_gibound_receptor.xtc' \
+	--out_plots 'plots/rhodopsin_receptor' \
+	--out_pc 'pca/rhodopsin_receptor' \
+	--start_frame 2000 \
+	--feature_type 'bb-torsions' \
+	--num_eigenvalues 12 \
+	--num_components 3 \
+	--feat_threshold 0.4 
 
--- tutorial/3-clustering.sh
+++ tutorial/3-clustering.sh
@@ -1,4 +1,18 @@
 mkdir -p clusters
 
-python ~/pensa/scripts/calculate_combined_clusters.py --no-write --wss
+python ~/pensa/scripts/calculate_combined_clusters.py --no-write --wss \
+	--ref_file_a 'traj/rhodopsin_arrbound_receptor.gro' \
+	--trj_file_a 'traj/rhodopsin_arrbound_receptor.xtc' \
+	--ref_file_b 'traj/rhodopsin_gibound_receptor.gro' \
+	--trj_file_b 'traj/rhodopsin_gibound_receptor.xtc' \
+	--label_a 'Rho-Arr' \
+	--label_b 'Rho-Gi' \
+	--out_plots 'plots/rhodopsin_receptor' \
+	--out_frames_a 'clusters/rhodopsin_arrbound_receptor' \
+	--out_frames_b 'clusters/rhodopsin_gibound_receptor' \
+	--start_frame 2000 \
+	--feature_type 'bb-torsions' \
+	--algorithm 'kmeans' \
+	--max_num_clusters 12 \
+	--write_num_clusters 2
 
