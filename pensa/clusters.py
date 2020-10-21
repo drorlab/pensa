164ec609b09f8a7a70ed4389a8a6ce8e865414aa
Martin Voegele <martinvoegele1989@gmail.com>
2020-10-21 00:03:38 -0700

    harmonize docstrings and internal variables

--- pensa/clusters.py
+++ pensa/clusters.py
@@ -12,24 +12,30 @@ import matplotlib.pyplot as plt
 # --- METHODS FOR CLUSTERING ---
     
     
-def obtain_clusters(data,algorithm='kmeans',num_clusters=2,min_dist=12,max_iter=100,
-                    plot=True,saveas=None):
-    '''Performs a PyEMMA clustering on the provided data
+def obtain_clusters(data, algorithm='kmeans', 
+                    num_clusters=2, min_dist=12, max_iter=100,
+                    plot=True, saveas=None):
+    """
+    Clusters the provided data.
     
-    Parameters
-    ----------
-    data: float array.
-        Trajectory data [frames,frame_data]
-    algorithm: string
-        The algorithm to use for the clustering. 
-        Options: kmeans, rspace. Default: kmeans
-    num_clusters: int.
-        Number of clusters for k-means clustering
-    min_dist: float.
-        Minimum distance for regspace clustering
-    max_iter (opt.): int.
-        Maximum number of iterations.
-    '''
+    Args:
+        data (float array): Trajectory data [frames,frame_data]
+        algorithm (string): The algorithm to use for the clustering. 
+            Options: kmeans, rspace. 
+            Default: kmeans
+        num_clusters (int, optional): Number of clusters for k-means clustering. 
+            Default: 2.
+        min_dist (float, optional): Minimum distance for regspace clustering. 
+            Default: 12.
+        max_iter (int, optional): Maximum number of iterations. 
+            Default: 100.
+        
+    Returns:
+        cidx (int array): Cluster indices for each frame.
+        total_wss (float): With-in-sum-of-squares (WSS).
+        centroids (float array): Centroids for all the clusters.
+    
+    """
     
     # Perform PyEMMA clustering
     assert algorithm in ['kmeans','rspace']
@@ -65,43 +71,49 @@ def obtain_clusters(data,algorithm='kmeans',num_clusters=2,min_dist=12,max_iter=
     return cidx, total_wss, centroids
 
 
-def obtain_combined_clusters(data_g, data_a, start_frame, label_g, label_a,
-                             algorithm='kmeans',num_clusters=2,min_dist=12,max_iter=100,plot=True,saveas=None):
-    '''Performs a PyEMMA clustering on a combination of two data sets.
+def obtain_combined_clusters(data_a, data_b, label_a = 'Sim A', label_b = 'Sim B', start_frame = 0,
+                             algorithm='kmeans', num_clusters=2, min_dist=12, max_iter=100,
+                             plot=True, saveas=None):
+    """
+    Clusters a combination of two data sets.
     
-    Parameters
-    ----------
-    data_g: float array.
-        Trajectory data [frames,frame_data]
-    data_a: float array.
-        Trajectory data [frames,frame_data]
-    start_frame: int.
-        Frame from which the clustering data starts.
-    label_g: str.
-        Label for the plot.
-    label_a: str.
-        Label for the plot.
-    algorithm: string
-        The algorithm to use for the clustering. 
-        Options: kmeans, rspace. Default: kmeans
-    num_clusters: int.
-        Number of clusters for k-means clustering
-    min_dist: float.
-        Minimum distance for regspace clustering
-    max_iter (opt.): int.
-        Maximum number of iterations.
-    saveas (opt.): str.
-        Name of the file to save the plot
-    '''
+    Args:
+        data_a (float array): Trajectory data [frames,frame_data]
+        data_b (float array): Trajectory data [frames,frame_data]
+        label_a (str, optional): Label for the plot.
+            Default: Sim A.
+        label_b (str, optional): Label for the plot.
+            Default: Sim B.
+        start_frame (int): Frame from which the clustering data starts.
+            Default: 0.
+        algorithm (string): The algorithm to use for the clustering. 
+            Options: kmeans, rspace. 
+            Default: kmeans
+        num_clusters (int, optional): Number of clusters for k-means clustering. 
+            Default: 2.
+        min_dist (float, optional): Minimum distance for regspace clustering. 
+            Default: 12.
+        max_iter (int, optional): Maximum number of iterations. 
+            Default: 100.
+        saveas (str, optional): Name of the file to save the plot.
+        
+    Returns:
+        cidx (int array): Cluster indices for each frame.
+        cond (int array): Index of the simulation the data came frome.
+        oidx (int array): Index of each frame in the original simulation (taking into account cutoff)
+        total_wss (float): With-in-sum-of-squares (WSS).
+        centroids (float array): Centroids for all the clusters.
+           
+    """
     
     # Combine the data
-    data = np.concatenate([data_g,data_a],0)
+    data = np.concatenate([data_a,data_b],0)
 
     # Remember which simulation the data came frome
-    cond = np.concatenate([np.ones(len(data_g)), np.zeros(len(data_a))])
+    cond = np.concatenate([np.ones(len(data_a)), np.zeros(len(data_b))])
 
     # Remember the index in the respective simulation (taking into account cutoff)
-    oidx = np.concatenate([np.arange(len(data_g))+start_frame, np.arange(len(data_a))+start_frame])
+    oidx = np.concatenate([np.arange(len(data_a))+start_frame, np.arange(len(data_b))+start_frame])
 
     # Perform PyEMMA clustering
     assert algorithm in ['kmeans','rspace']
@@ -130,10 +142,10 @@ def obtain_combined_clusters(data_g, data_a, start_frame, label_g, label_a,
     if plot:
         fig,ax = plt.subplots(1,1,figsize=[4,3],sharex=True,dpi=300)
         c, nc   = np.unique(cidx,return_counts=True)
-        cg, ncg = np.unique(cidx[cond==1],return_counts=True)
-        ca, nca = np.unique(cidx[cond==0],return_counts=True)
-        ax.bar(cg-0.15,ncg,0.3,label=label_g)
-        ax.bar(ca+0.15,nca,0.3,label=label_a)
+        ca, nca = np.unique(cidx[cond==1],return_counts=True)
+        cb, ncb = np.unique(cidx[cond==0],return_counts=True)
+        ax.bar(ca-0.15,nca,0.3,label=label_a)
+        ax.bar(cb+0.15,ncb,0.3,label=label_b)
         ax.legend()
         ax.set_xticks(c)
         ax.set_xlabel('clusters')
@@ -145,45 +157,84 @@ def obtain_combined_clusters(data_g, data_a, start_frame, label_g, label_a,
     return cidx, cond, oidx, total_wss, centroids
 
 
-def write_cluster_traj(cluster_idx, top_file, trj_file, out_name, start_frame):
-    '''Writes a trajectory into a separate file for each cluster.'''
+def write_cluster_traj(cluster_idx, top_file, trj_file, out_name, start_frame=0):
+    """
+    Writes a trajectory into a separate file for each cluster.
     
+    Args:
+        cluster_idx (int array): Cluster index for each frame.
+        top_file (str): Reference topology for the second trajectory. 
+        trj_file (str): Trajetory file from which the frames are picked.
+        out_name (str): Core part of the name of the output files.
+        start_frame (int, optional): Frame from which to start reading the trajectory.
+        
+    """
+    
+    # Load and select the protein
     u = mda.Universe(top_file, trj_file)
     protein = u.select_atoms('all')
     
+    # Loop over clusters
     num_clusters = np.max(cluster_idx)+1
-    
     for nr in range(num_clusters):
+        # For each cluster, write the corresponding frames to their new trajectory.
         with mda.Writer(out_name+"_c"+str(nr)+".xtc", protein.n_atoms) as W:
             for ts in u.trajectory:
                 if ts.frame >= start_frame and cluster_idx[ts.frame-start_frame] == nr: 
                     W.write(protein)
+    return
                     
 
 def wss_over_number_of_clusters(data, algorithm='kmeans', 
-                                max_iter=100, num_repeats = 5, max_num_clusters = 12, 
+                                max_iter=100, num_repeats = 5, max_num_clusters = 12,
                                 plot_file = None):
-    '''Calculates the Within-Sum-of-Squares for different numbers of clusters,
-       averaged over several iterations.'''
+    """
+    Calculates the within-sum-of-squares (WSS) for different numbers of clusters,
+    averaged over several iterations.
     
+    Args:
+        data (float array): Trajectory data [frames,frame_data]
+        algorithm (string): The algorithm to use for the clustering. 
+            Options: kmeans, rspace. 
+            Default: kmeans
+        max_iter (int, optional): Maximum number of iterations. 
+            Default: 100.
+        num_repeats (int, optional): Number of times to run the clustering for each number of clusters.
+            Default: 5.
+        max_num_clusters (int, optional): Maximum number of clusters for k-means clustering. 
+            Default: 12.
+        plot_file (str, optional): Name of the file to save the plot.
+        
+    Returns:
+        all_wss (float array): WSS values for each number of clusters (starting at 2).
+        std_wss (float array): standard deviations of the WSS.
+        
+    """
+    
+    # Initialize lists
     all_wss = []
     std_wss = []
+    # Loop over the number of clusters
     for nc in range(1,max_num_clusters):
         rep_wss = []
+        # Run each clustering several times.
         for repeat in range(num_repeats):
+            # Get clusters and WSS for this repetition.
             cc = obtain_clusters(data, algorithm=algorithm, max_iter=max_iter, 
                                  num_clusters=nc, plot=False)
             cidx, wss, centroids = cc
             rep_wss.append(wss)
-
+        # Calculate mean and standard deviation for this number of clusters.
         all_wss.append(np.mean(rep_wss))
         std_wss.append(np.std(rep_wss))
-        
+    
+    # Plot the WSS over the number of clusters
     fig, ax = plt.subplots(1,1, figsize=[4,3], dpi=300)
     ax.errorbar(np.arange(len(all_wss))+2,np.array(all_wss),yerr=np.array(std_wss)/np.sqrt(num_repeats))
     ax.set_xlabel('number of clusters')
     ax.set_ylabel('total WSS')
     fig.tight_layout()
+    # Save the plot to file.
     if plot_file: fig.savefig(plot_file)
     
     return all_wss, std_wss
@@ -192,33 +243,59 @@ def wss_over_number_of_clusters(data, algorithm='kmeans',
 def wss_over_number_of_combined_clusters(data_a, data_b, label_a = 'Sim A', label_b = 'Sim B', start_frame = 0, 
                                          algorithm='kmeans', max_iter=100, num_repeats = 5, max_num_clusters = 12, 
                                          plot_file = None):
-    '''Calculates the Within-Sum-of-Squares for different numbers of clusters,
-       averaged over several iterations.'''
+    """
+    Calculates the Within-Sum-of-Squares for different numbers of clusters,
+    averaged over several iterations.
     
+    Args:
+        data_a (float array): Trajectory data [frames,frame_data]
+        data_b (float array): Trajectory data [frames,frame_data]
+        label_a (str, optional): Label for the plot.
+        label_b (str, optional): Label for the plot.
+        start_frame (int, optional): Frame from which the clustering data starts.
+        algorithm (string): The algorithm to use for the clustering. 
+            Options: kmeans, rspace. 
+            Default: kmeans
+        max_iter (int, optional): Maximum number of iterations.
+            Default: 100.
+        num_repeats (int, optional): Number of times to run the clustering for each number of clusters.
+            Default: 5.
+        max_num_clusters (int, optional): Maximum number of clusters for k-means clustering.
+            Default: 12.
+        plot_file (str, optional): Name of the file to save the plot.
+        
+    Returns:
+        all_wss (float array): WSS values for each number of clusters (starting at 2).
+        std_wss (float array): standard deviations of the WSS.
+    
+    """
+    
+    # Initialize lists
     all_wss = []
     std_wss = []
+    # Loop over the number of clusters
     for nc in range(1,max_num_clusters):
         rep_wss = []
+        # Run each clustering several times.
         for repeat in range(num_repeats):
-            cc = obtain_combined_clusters(data_a, data_b, start_frame, 
-                                          label_a, label_b,  
-                                          algorithm=algorithm, max_iter=max_iter, 
-                                          num_clusters=nc, plot=False)
+            # Get clusters and WSS for this repetition.
+            cc = obtain_combined_clusters(data_a, data_b, label_a, label_b, start_frame, 
+                                          algorithm=algorithm, max_iter=max_iter, num_clusters=nc, 
+                                          plot=False)
             cidx, cond, oidx, wss, centroids = cc
             rep_wss.append(wss)
-
+        # Calculate mean and standard deviation for this number of clusters.
         all_wss.append(np.mean(rep_wss))
         std_wss.append(np.std(rep_wss))
         
+    # Plot the WSS over the number of clusters
     fig, ax = plt.subplots(1,1, figsize=[4,3], dpi=300)
     ax.errorbar(np.arange(len(all_wss))+2,np.array(all_wss),yerr=np.array(std_wss)/np.sqrt(num_repeats))
     ax.set_xlabel('number of clusters')
     ax.set_ylabel('total WSS')
     fig.tight_layout()
+    # Save the plot to file.
     if plot_file: fig.savefig(plot_file)
     
     return all_wss, std_wss
-    
-    
 
-                    
--- pensa/pca.py
+++ pensa/pca.py
@@ -1,7 +1,4 @@
 import numpy as np
-import scipy as sp
-import scipy.stats
-import mdshare
 import pyemma
 from pyemma.util.contexts import settings
 import MDAnalysis as mda
@@ -13,47 +10,54 @@ import matplotlib.pyplot as plt
 
 
 def calculate_pca(data):
-    '''Performs a PyEMMA PCA on the provided data
+    """
+    Performs a PyEMMA PCA on the provided data.
     
-    Parameters
-    ----------
-    data: float array.
-        Trajectory data [frames,frame_data]
-    '''
-    
+    Args:
+        data (float array): Trajectory data [frames,frame_data].
+        
+    Returns:
+        pca (PCA obj): Principal components information.
+        
+    """
     pca = pyemma.coordinates.pca(data)
-    
     return pca
 
 
 def pca_eigenvalues_plot(pca, num=12, plot_file=None):
-
+    """
+    Plots the highest eigenvalues over the numberr of the principal components.
+    
+    Args:
+        pca (PCA obj): Principal components information.
+        num (int, optional): Number of eigenvalues to plot. Defaults to 12.
+        plot_file(str, optional): Path and name of the file to save the plot.
+        
+    """
+    # Plot eigenvalues over component numbers
     fig,ax = plt.subplots(1, 1, figsize=[4,3], dpi=300)
     ax.plot(np.arange(num)+1, pca.eigenvalues[:num], 'o')
     ax.set_xlabel('component number')
     ax.set_ylabel('eigenvalue')
     fig.tight_layout()
-
-    if plot_file: fig.savefig(plot_file,dpi=300)
-
+    # Save the figure to a file
+    if plot_file: fig.savefig(plot_file, dpi=300)
     return 
 
 
 def pca_features(pca, features, num, threshold, plot_file=None):
-    '''Prints relevant features and plots feature correlations.
+    """
+    Prints relevant features and plots feature correlations.
     
-    Parameters
-    ----------
-    pca: pyemma PCA object.
-        The PCA of which to plot the features.
-    features: list of strings
-        Features for which the PCA was performed (obtained from features object via .describe()).
-    num: float.
-        Number of feature correlations to plot.
-    threshold: float.
-        Features with a correlation above this will be printed.
-    '''
-    
+    Args:
+        pca (PCA obj): The PCA of which to plot the features.
+        features (list of str): Features for which the PCA was performed.
+            (obtained from features object via .describe()).
+        num (float): Number of feature correlations to plot.
+        threshold (float): Features with a correlation above this will be printed.
+        plot_file(str, optional): Path and name of the file to save the plot.
+        
+    """
     # Plot the highest PC correlations and print relevant features
     fig,ax = plt.subplots(num,1,figsize=[4,num*3],dpi=300,sharex=True)
     for i in range(num):
@@ -63,177 +67,156 @@ def pca_features(pca, features, num, threshold, plot_file=None):
             if relevant[j]: print(ft, "%6.3f"%(pca.feature_PC_correlation[j,i]))
         ax[i].plot(pca.feature_PC_correlation[:,i])
     fig.tight_layout()
-
+    # Save the figure to a file
     if plot_file: fig.savefig(plot_file,dpi=300)
-
     return 
     
     
 def project_on_pc(data, ev_idx, pca=None):
-    '''Projects a trajectory onto an eigenvector of its PCA.
+    """
+    Projects a trajectory onto an eigenvector of its PCA.
     
-    Parameters
-    ----------
-    data: float array.
-        Trajectory data [frames,frame_data]
-    ev_idx: int
-        Index of the eigenvector to project on
-    pca (opt.): pyemma PCA object.
-        Pre-calculated PCA. Must be calculated for the same features (but not necessarily the same trajectory)
-    '''
+    Args:
+        data (float array): Trajectory data [frames,frame_data].
+        ev_idx (int): Index of the eigenvector to project on.
+        pca (PCA obj, optional): Information of pre-calculated PCA. Defaults to None.
+            Must be calculated for the same features (but not necessarily the same trajectory).
     
+    Returns:
+        projection (float array): value along the PC for each frame.
+        
+    """
+    # Perform PCA if none is provided
     if pca is None:
         pca = pyemma.coordinates.pca(data) #,dim=3)
-
+    # Project the features onto the principal components
     projection = np.zeros(data.shape[0])
     for ti in range(data.shape[0]):
         projection[ti] = np.dot(data[ti],pca.eigenvectors[:,ev_idx])
-        
+    # Return the value along the PC for each frame  
     return projection
     
 
 def sort_traj_along_pc(data, pca, start_frame, top, trj, out_name, num_pc=3):
-    '''Sort a trajectory along given principal components.
+    """
+    Sort a trajectory along given principal components.
     
-    Parameters
-    ----------
-    data: float array.
-        Trajectory data [frames,frame_data]
-    pca: PCA object.
-        principal compoenents information
-    num_pc: int
-        sort along the first num_pc principal components
-    start_frame: int
-        offset of the data with respect to the trajectories (defined below)
-    top: string.
-        reference topology for the trajectory. 
-    trj: string.
-        first of the trajetories from which the frames are picked. 
-        Should be the same as data was from.
-    out_name: string.
-        core part of the name of the output files
-    '''
+    Args:
+        data (float array): Trajectory data [frames,frame_data].
+        pca (PCA obj): Principal components information.
+        num_pc (int): Sort along the first num_pc principal components.
+        start_frame (int): Offset of the data with respect to the trajectories (defined below).
+        top (str): File name of the reference topology for the trajectory. 
+        trj (str): File name of the trajetory from which the frames are picked. 
+            Should be the same as data was from.
+        out_name (str): core part of the name of the output files
     
+    """    
     # Remember the index in the simulation (taking into account cutoff)
     oidx = np.arange(len(data))+start_frame
-
     # Define the MDAnalysis trajectories from where the frames come
     u = mda.Universe(top,trj)
     a = u.select_atoms('all')
-
+    # Loop through the principal components
     for evi in range(num_pc):
-
         # Project the combined data on the principal component
         proj = project_on_pc(data,evi,pca=pca)
-
-        # Sort everything along the projection on th resp. PC
+        # Sort everything along the projection onto the PC
         sort_idx  = np.argsort(proj)
         proj_sort = proj[sort_idx] 
         oidx_sort = oidx[sort_idx]
-
+        # Write the trajectory, ordered along the PC
         with mda.Writer(out_name+"_pc"+str(evi)+".xtc", a.n_atoms) as W:
             for i in range(data.shape[0]):
                 ts = u.trajectory[oidx_sort[i]]
                 W.write(a)
-                
+    return
 
-def sort_trajs_along_common_pc(data_g, data_a, start_frame, top_g, top_a, trj_g, trj_a, out_name, num_pc=3):
-    '''Sort two trajectories along the 12 highest principal components.
+
+def sort_trajs_along_common_pc(data_a, data_b, start_frame, top_a, top_b, trj_a, trj_b, out_name, num_pc=3):
+    """
+    Sort two trajectories along their most important common principal components.
     
-    Parameters
-    ----------
-    data_g: float array.
-        Trajectory data [frames,frame_data]
-    data_a: float array.
-        Trajectory data [frames,frame_data]
-    start_frame: int
-        offset of the data with respect to the trajectories (defined below)
-    top_g: string.
-        reference topology for the first trajectory (g-bound). 
-    top_a: string.
-        reference topology for the second trajectory (arr-bound). 
-    trj_g: string.
-        first of the trajetories from which the frames are picked (g-bound). 
-        Should be the same as data_g was from.
-    trj_a: string.
-        second of the trajetories from which the frames are picked (arr-bound). 
-        Should be the same as data_g was from.
-    out_name: string.
-        core part of the name of the output files
-    '''
-
-    # Combine the data
-    data = np.concatenate([data_g,data_a],0)
-
+    Args:
+        data_a (float array): Trajectory data [frames,frame_data].
+        data_b (float array): Trajectory data [frames,frame_data].
+        start_frame (int): Offset of the data with respect to the trajectories (defined below).
+        top_a (str): Reference topology for the first trajectory. 
+        top_b (str): Reference topology for the second trajectory. 
+        trj_a (str): First of the trajetories from which the frames are picked. 
+            Should be the same as data_a was from.
+        trj_b (str): Second of the trajetories from which the frames are picked. 
+            Should be the same as data_b was from.
+        out_name (str): Core part of the name of the output files.
+    
+    """
+    # Combine the input data
+    data = np.concatenate([data_a,data_b],0)
     # Remember which simulation the data came frome
-    cond = np.concatenate([np.ones(len(data_g)), np.zeros(len(data_a))])
-
+    cond = np.concatenate([np.ones(len(data_a)), np.zeros(len(data_b))])
     # Remember the index in the respective simulation (taking into account cutoff)
-    oidx = np.concatenate([np.arange(len(data_g))+start_frame, 
-                           np.arange(len(data_a))+start_frame])
-
+    oidx = np.concatenate([np.arange(len(data_a))+start_frame, 
+                           np.arange(len(data_b))+start_frame])
     # Calculate the principal components
     pca = pyemma.coordinates.pca(data,dim=3)
-
     # Define the MDAnalysis trajectories from where the frames come
-    ug = mda.Universe(top_g,trj_g)
     ua = mda.Universe(top_a,trj_a)
-
-    ag = ug.select_atoms('all')
+    ub = mda.Universe(top_b,trj_b)
+    # ... and select all atoms
     aa = ua.select_atoms('all')
-
+    ab = ub.select_atoms('all')
+    # Loop over principal components.
     for evi in range(num_pc):
-
         # Project the combined data on the principal component
         proj = project_on_pc(data,evi,pca=pca)
-
         # Sort everything along the projection on th resp. PC
         sort_idx  = np.argsort(proj)
         proj_sort = proj[sort_idx] 
         cond_sort = cond[sort_idx]
         oidx_sort = oidx[sort_idx]
-
-        with mda.Writer(out_name+"_pc"+str(evi)+".xtc", ag.n_atoms) as W:
+        # Write the trajectory, ordered along the PC
+        with mda.Writer(out_name+"_pc"+str(evi)+".xtc", aa.n_atoms) as W:
             for i in range(data.shape[0]):
                 if cond_sort[i] == 1: # G-protein bound
-                    ts = ug.trajectory[oidx_sort[i]]
-                    W.write(ag)
-                elif cond_sort[i] == 0: # arrestin bound
                     ts = ua.trajectory[oidx_sort[i]]
                     W.write(aa)
+                elif cond_sort[i] == 0: # arrestin bound
+                    ts = ub.trajectory[oidx_sort[i]]
+                    W.write(ab)
+    return
 
 
-def compare_projections(data_g, data_a, pca, num=3, saveas=None):
-    '''Compare two datasets along a given principal component.
+def compare_projections(data_a, data_b, pca, num=3, saveas=None, label_a=None, label_b=None):
+    """
+    Compare two datasets along a given principal component.
     
-    Parameters
-    ----------
-    data_g: float array.
-        Trajectory data [frames,frame_data]
-    data_a: float array.
-        Trajectory data [frames,frame_data]
-    pca: PCA object.
-        Principal components information.
-    num: int.
-        Number of princibal components to plot. 
-    saveas: string.
-        Name of the output file.
-    '''
-    
-    fig,ax = plt.subplots(num,2,figsize=[8,3*num],dpi=300)
-
+    Args:
+        data_a (float array): Trajectory data [frames,frame_data]
+        data_b (float array): Trajectory data [frames,frame_data]
+        pca (PCA object): Principal components information.
+        num (int): Number of principal components to plot. 
+        saveas (str, optional): Name of the output file.
+        
+    """
+    # Start the figure    
+    fig,ax = plt.subplots(num, 2, figsize=[8,3*num], dpi=300)
+    # Loop over PCs
     for evi in range(num):
-        
-        proj_a = project_on_pc(data_a,evi,pca=pca)
-        proj_g = project_on_pc(data_g,evi,pca=pca)
-
-        ax[evi,0].plot(proj_a,alpha=0.5)
-        ax[evi,0].plot(proj_g,alpha=0.5)
-
-        ax[evi,1].hist(proj_a,bins=30,alpha=0.5,density=True)
-        ax[evi,1].hist(proj_g,bins=30,alpha=0.5,density=True)
-    
+        # Calculate values along PC for each frame
+        proj_a = project_on_pc(data_a, evi, pca=pca)
+        proj_b = project_on_pc(data_b, evi, pca=pca)
+        # Plot the time series in the left panel
+        ax[evi,0].plot(proj_a, alpha=0.5, label=label_a)
+        ax[evi,0].plot(proj_b, alpha=0.5, label=label_b)
+        # Plot the histograms in the right panel
+        ax[evi,1].hist(proj_a, bins=30, alpha=0.5, density=True, label=label_a)
+        ax[evi,1].hist(proj_b, bins=30, alpha=0.5, density=True, label=label_b)
+        # Legend
+        if label_a and label_b:
+            ax[evi,0].legend()
+            ax[evi,1].legend()
+    # Save the figure
     if saveas is not None:
-        fig.savefig(saveas,dpi=300)
-        
-        
+        fig.savefig(saveas, dpi=300)
+    return
+
