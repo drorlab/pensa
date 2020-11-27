bb800cf2ef88b8f6a18777a12ee3114f777c08a6
Martin Voegele <martinvoegele1989@gmail.com>
2020-11-27 03:35:45 -0800

    changes for PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 from setuptools import setup
 
 setup(name='pensa',
-      version='0.1',
+      version='0.1.0',
       description='PENSA - protein ensemble analysis',
       url='http://github.com/drorlab/pensa',
       author='Martin Voegele',
-      author_email='martin.voegele@stanford.edu',
+      author_email='mvoegele@stanford.edu',
       license='MIT',
       packages=['pensa'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[
+        'numpy',
+        'scipy>=1.2',
+        'mdshare',
+        'pyemma',
+        'MDAnalysis',
+        'matplotlib'
+      ],
+      classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+        # license (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+        # Supported Python versions
+        'Programming Language :: Python :: 3',
+      ],)
 
