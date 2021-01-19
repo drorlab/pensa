720e7c4febcbeb82e571e8e3d1f27c603c0da382
neilsyt <njthomson@dundee.ac.uk>
2021-01-19 15:24:55 +0000

    updating the __init__ to initialise new scripts

--- pensa/__init__.py
+++ pensa/__init__.py
@@ -7,9 +7,12 @@ from pyemma.util.contexts import settings
 import MDAnalysis as mda
 import matplotlib.pyplot as plt
 
+
+
 from .features import *
 from .clusters import *
 from .comparison import *  
 from .pca import *
 from .preprocessing import *
-
+from .statesinfo import *
+from .density_features import *
