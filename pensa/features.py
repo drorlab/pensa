527284901205e87ec47427561bfddfb8cda91431
martinvoegele <martinvoegele1989@gmail.com>
2021-02-01 17:16:40 -0800

    no cos/sin for torsions and no distances by default

--- pensa/features.py
+++ pensa/features.py
@@ -7,21 +7,33 @@ from pyemma.util.contexts import settings
 # -- Loading the Features --
 
 
-def get_features(pdb, xtc, start_frame=0, step_width=1, 
-                 features=['bb-torsions', 'sc-torsions', 'bb-distances']):
+def get_features(pdb, xtc, features=['bb-torsions', 'sc-torsions'],
+                 start_frame=0, step_width=1, cossin=False):
     """
     Load the features. Currently implemented: bb-torsions, sc-torsions, bb-distances
     http://www.emma-project.org/latest/api/generated/pyemma.coordinates.featurizer.html
     
-    Args:
-        pdb (str): File name for the reference file (PDB or GRO format).
-        xtc (str): File name for the trajectory (xtc format).
-        start_frame (int, optional): First frame to return of the features. Already takes subsampling by stride>=1 into account. Defaults to 0.
-        step_width (int, optional): Subsampling step width when reading the frames. Defaults to 1.
-    
-    Returns:
-        feature_names (list of str): Names of all features
-        features_data (numpy array): Data for all features
+    Parameters
+    ----------
+        pdb : str
+            File name for the reference file (PDB or GRO format).
+        xtc : str
+            File name for the trajectory (xtc format).
+        features : list of str, default=['bb-torsions', 'sc-torsions']
+            Names of the features to be extracted.
+        start_frame : int, default=0
+            First frame to return of the features. Already takes subsampling by stride>=1 into account.
+        step_width : int, default=1
+            Subsampling step width when reading the frames. 
+        cossin : bool, default=False
+            Use cosine and sine for angles.
+        
+    Returns
+    -------
+        feature_names : list of str
+            Names of all features
+        features_data : numpy array
+            Data for all features
     
     """
     # Initialize the dictionaries.
@@ -30,14 +42,14 @@ def get_features(pdb, xtc, start_frame=0, step_width=1,
     # Add backbone torsions.
     if 'bb-torsions' in features:
         bbtorsions_feat = pyemma.coordinates.featurizer(pdb)
-        bbtorsions_feat.add_backbone_torsions(cossin=True, periodic=False)
+        bbtorsions_feat.add_backbone_torsions(cossin=cossin, periodic=False)
         bbtorsions_data = pyemma.coordinates.load(xtc, features=bbtorsions_feat, stride=step_width)[start_frame:]
         feature_names['bb-torsions'] = bbtorsions_feat.describe()
         features_data['bb-torsions'] = bbtorsions_data
     # Add sidechain torsions.
     if 'sc-torsions' in features:
         sctorsions_feat = pyemma.coordinates.featurizer(pdb)
-        sctorsions_feat.add_sidechain_torsions(cossin=True, periodic=False)
+        sctorsions_feat.add_sidechain_torsions(cossin=cossin, periodic=False)
         sctorsions_data = pyemma.coordinates.load(xtc, features=sctorsions_feat, stride=step_width)[start_frame:]
         feature_names['sc-torsions'] = sctorsions_feat.describe()
         features_data['sc-torsions'] = sctorsions_data
@@ -60,11 +72,15 @@ def remove_atom_numbers_from_distance(feat_str):
     """
     Remove atom numbers from a distance feature string.
     
-    Args:
-        feat_str (str): The string describing a single feature.
+    Parameters
+    ----------
+        feat_str : str
+            The string describing a single feature.
     
-    Returns:
-        new_feat (str): The feature string without atom numbers.
+    Returns
+    -------
+        new_feat : str
+            The feature string without atom numbers.
     
     """
     # Split the feature string in its parts
@@ -80,11 +96,15 @@ def describe_dist_without_atom_numbers(feature_names):
     """
     Provides feature descriptors without atom numbers.
     
-    Args:
-        feature_names (dict): Names of all features (assumes distances).
+    Parameters
+    ----------
+        feature_names : dict
+            Names of all features (assumes distances).
     
-    Returns:
-        desc (list of str): The feature descriptors without atom numbers.
+    Returns
+    -------
+        desc : list of str
+            The feature descriptors without atom numbers.
     
     """
     desc = feature_names.describe()
@@ -100,11 +120,15 @@ def sort_sincos_torsions_by_resnum(tors, data):
     """
     Sort sin/cos of torsion features by the residue number..
 
-    Args:
-        tors (str[]): The list of torsion features.
+    Parameters
+    ----------
+        tors : list of str
+            The list of torsion features.
 
-    Returns:
-        new_tors (str[]): The sorted list of torsion features.
+    Returns
+    -------
+        new_tors : list of str
+            The sorted list of torsion features.
 
     """
     renamed = []
@@ -123,11 +147,15 @@ def sort_distances_by_resnum(dist, data):
     """
     Sort distance features by the residue number..
 
-    Args:
-        dist (str[]): The list of distance features.
+    Parameters
+    ----------
+        dist : list of str
+            The list of distance features.
 
-    Returns:
-        new_dist (str[]): The sorted list of distance features.
+    Returns
+    -------
+        new_dist : list of str
+            The sorted list of distance features.
 
     """
     renamed = []
