dc0c5bd3af6852cb27e2fc62d6a62ee66aa52d65
Martin Vögele <martinvoegele@users.noreply.github.com>
2021-03-02 03:47:04 -0800

    Update preprocessing.py

--- pensa/preprocessing.py
+++ pensa/preprocessing.py
@@ -58,6 +58,7 @@ def load_selection(sel_file, sel_base=''):
             sel_string += range_to_string(*r)
     return sel_string
 
+
 def extract_coordinates(ref, pdb, trj_list, out_name, sel_string, start_frame=0,
                         rename_segments=None ):
     """
@@ -206,3 +207,33 @@ def download_from_gpcrmd(filename, folder):
     os.makedirs(folder, exist_ok=True)
     open(out, 'wb').write(req.content)
     return
+
+
+def get_transmem_from_uniprot(uniprot_id):
+    """
+    Retains transmembrane regions from Uniprot (first and last residue each).
+    This function requires internet access.
+    
+    Parameters
+    ----------
+        uniprot_id : str
+            The UNIPROT ID of the protein.
+        
+    Returns
+    -------
+        tm : list 
+            List of all transmembrane regions, represented as tuples with first and last residue ID.
+        
+    """
+    url = 'https://www.uniprot.org/uniprot/'+uniprot_id+'.txt'
+    r = requests.get(url, allow_redirects=True)
+    c = r.content
+    tm = []
+    for line in c.splitlines():
+        if line.startswith(b'FT   TRANSMEM'):
+            l = str(line)
+            l = l.replace('b\'FT   TRANSMEM        ','')
+            l = l.replace('\'','')
+            s = l.split('.')
+            tm.append((int(s[0]),int(s[-1])))
+    return tm
