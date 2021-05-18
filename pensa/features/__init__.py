b58613cac89456a51fe6e83f2b6d50fe3134b905
martinvoegele <martinvoegele1989@gmail.com>
2021-05-17 19:06:21 -0700

    more specific module name(s) for mda features

--- pensa/features/__init__.py
+++ pensa/features/__init__.py
@@ -5,7 +5,7 @@ Methods to read in and process features from coordinates.
 """
 
 from .pyemma_features import *
-from .mda_features import *
+from .mda_distances import *
 from .processing import *
 from .atom_features import *  
 from .water_features import *  
