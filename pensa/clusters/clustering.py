926455c2ca7cac881ad3ded6391d3b4d31f6d324
martinvoegele <martinvoegele1989@gmail.com>
2021-05-21 22:50:39 -0700

    clustering for arbitrary number of conditions

--- pensa/clusters/clustering.py
+++ pensa/clusters/clustering.py
@@ -192,6 +192,119 @@ def obtain_combined_clusters(data_a, data_b, label_a = 'Sim A', label_b = 'Sim B
             fig.savefig(saveas,dpi=300)
     
     return cidx, cond, oidx, total_wss, centroids
+    
+    
+def obtain_mult_combined_clusters(data, start_frame = 0, algorithm='kmeans', 
+                                  num_clusters=2, min_dist=12, max_iter=100,
+                                  plot=True, saveas=None, labels=None, colors=None):
+    """
+    Clusters a combination of two data sets.
+    
+    Parameters
+    ----------
+        data : list of float arrays
+            Trajectory data [frames,frame_data]
+        start_frame : int
+            Frame from which the clustering data starts.
+            Default: 0.
+        algorithm : string
+            The algorithm to use for the clustering. 
+            Options: kmeans, rspace. 
+            Default: kmeans
+        num_clusters : int, optional
+            Number of clusters for k-means clustering. 
+            Default: 2.
+        min_dist : float, optional
+            Minimum distance for regspace clustering. 
+            Default: 12.
+        max_iter : int, optional
+            Maximum number of iterations. 
+            Default: 100.
+        plot : bool, optional
+            Create a plot. 
+            Default: True
+        saveas : str, optional
+            Name of the file in which to save the plot.
+            (only needed if "plot" is True)
+        labels : list of str, optional
+            Label for the plot.
+            Default: None.
+        colors : list of str, optional
+            Colors for the plot.
+            Default: None.
+        
+    Returns
+    -------
+        cidx : int array
+            Cluster indices for each frame.
+        cond : int array
+            Index of the simulation the data came frome.
+        oidx : int array
+            Index of each frame in the original simulation (taking into account cutoff)
+        total_wss : float
+            With-in-sum-of-squares (WSS).
+        centroids : float array
+            Centroids for all the clusters.
+           
+    """
+    if labels is not None:
+        assert len(labels) == len(data)
+    else:
+        labels = [None for _ in range(len(data))]
+    if colors is not None:
+        assert len(colors) == len(data)
+    else:
+        colors = ['C%i'%num for num in range(len(data))]
+    
+    num_frames = [len(d) for d in data]
+    num_traj = len(data)
+    # Combine the input data
+    data = np.concatenate(data,0)
+    # Remember which simulation the data came frome
+    cond = np.concatenate([i*np.ones(num_frames[i],dtype=int) for i in range(num_traj)])
+    # Remember the index in the respective simulation (taking into account cutoff)
+    oidx = np.concatenate([np.arange(num_frames[i])+start_frame for i in range(num_traj)])
+
+    # Perform PyEMMA clustering
+    assert algorithm in ['kmeans','rspace']
+    if algorithm == 'kmeans':
+        clusters = pyemma.coordinates.cluster_kmeans(data,k=num_clusters,max_iter=100)
+    elif algorithm == 'rspace':
+        clusters = pyemma.coordinates.cluster_regspace(data,min_dist)
+
+    # Extract cluster indices
+    cidx = clusters.get_output()[0][:,0]
+
+    # Calculate centroids and total within-cluster sum of square
+    centroids = []
+    total_wss = 0
+    for i in np.unique(cidx):
+        # get the data for this cluster
+        cluster_data = data[np.where(cidx==i)]
+        # calcualte the centroid
+        cluster_centroid = np.mean(cluster_data,0)
+        centroids.append(cluster_centroid)
+        # calculate the within-cluster sum of square
+        cluster_wss = np.sum( (cluster_data - cluster_centroid)**2 )
+        total_wss += cluster_wss
+    
+    # Count and plot
+    if plot:
+        fig,ax = plt.subplots(1,1,figsize=[4,3],sharex=True,dpi=300)
+        c, nc   = np.unique(cidx,return_counts=True)
+        for n in range(num_traj):
+            ca, nca = np.unique(cidx[cond==n],return_counts=True)
+            ax.bar(ca-0.2+n*0.4/num_traj,nca,0.4/num_traj,
+                   label=labels[n], color=colors[n])
+        ax.legend()
+        ax.set_xticks(c)
+        ax.set_xlabel('clusters')
+        ax.set_ylabel('population')
+        fig.tight_layout()
+        if saveas is not None:
+            fig.savefig(saveas,dpi=300)
+    
+    return cidx, cond, oidx, total_wss, centroids
 
 
 
