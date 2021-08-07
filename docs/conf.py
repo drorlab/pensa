ab02046ee6cec44c4214b4bcef15780ad2eba898
Martin Vögele <martinvoegele@users.noreply.github.com>
2021-08-07 15:33:30 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../pensa'))
 sys.path.insert(0, os.path.abspath('..'))
 import sphinx_rtd_theme
+import pensa
 
 
 # -- Project information -----------------------------------------------------
