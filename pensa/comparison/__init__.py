ce42b31ae390767866dbe091229488df58190afb
martinvoegele <martinvoegele1989@gmail.com>
2022-02-03 22:43:00 -0800

    move uncertainty analysis to comparison submodule

--- pensa/comparison/__init__.py
+++ pensa/comparison/__init__.py
@@ -2,4 +2,5 @@ from .statistics import *
 from .relative_entropy import *  
 from .statespecific import *  
 from .visualization import *  
-from .metrics import * 
+from .metrics import *
+from .uncertainty_analysis import *
