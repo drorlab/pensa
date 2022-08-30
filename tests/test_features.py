a16df119f20ab35dd885212af272f821543c359e
martinvoegele <martinvoegele1989@gmail.com>
2022-08-30 05:22:03 +0200

    create simple test functions for nocleic acid torsion readers

--- tests/test_features.py
+++ tests/test_features.py
@@ -24,12 +24,12 @@ trj_file_b = [root_dir_b+'/mor-bu72-1.xtc',
 
 # Simulation O (DNA-opt)
 root_dir_o = test_data_path+'/DNA-opt'
-ref_file_o = root_dir_o+'/DNA-nw.gro'
+gro_file_o = root_dir_o+'/DNA-nw.gro'
 trj_file_o = root_dir_o+'/trajfit-nw_step100.xtc'
 
 # Simulation S (DNA-std)
 root_dir_s = test_data_path+'/DNA-std'
-ref_file_s = root_dir_s+'/DNA-nw.gro'
+gro_file_s = root_dir_s+'/DNA-nw.gro'
 trj_file_s = root_dir_s+'/trajfit-nw_step100.xtc'
 
 # Test protein backbone torsions
@@ -72,12 +72,31 @@ def test_get_protein_sidechain_torsions():
     assert '%1.4f %1.4f'%(sc_torsions_a[1][0][0], sc_torsions_a[1][0][-1]) == '1.0151 -0.2732'
     assert '%1.4f %1.4f'%(sc_torsions_a[1][-1][0], sc_torsions_a[1][-1][-1]) == '-0.7536 0.1536'
 
-# TODO: Test nucleic-acid backbone torsions
+# Test nucleic acid backbone torsions
+def test_get_nucleicacid_backbone_torsions():
+    bb_torsions_o = get_nucleicacid_backbone_torsions(
+        gro_file_o, trj_file_o, selection='all',
+        first_frame=0, last_frame=None, step=1, 
+        naming='segindex', radians=True
+        )
+    bb_torsions_s = get_nucleicacid_backbone_torsions(
+        gro_file_s, trj_file_s, selection='all',
+        first_frame=0, last_frame=None, step=1, 
+        naming='segindex', radians=True
+        )
+    assert bb_torsions_o[0] == bb_torsions_s[0]
 
-# TODO: Test nucleic-acid pseudotorsions
-
-# Tests
-def test_get_features():
-    assert 4 == 4
-    pass
+# Test nucleic acid pseudo-torsions
+def test_get_nucleicacid_pseudotorsions():
+    pseudotorsions_o = get_nucleicacid_pseudotorsions(
+        gro_file_o, trj_file_o, selection='all',
+        first_frame=0, last_frame=None, step=1, 
+        naming='segindex', radians=True
+        )
+    pseudotorsions_s = get_nucleicacid_pseudotorsions(
+        gro_file_s, trj_file_s, selection='all',
+        first_frame=0, last_frame=None, step=1, 
+        naming='segindex', radians=True
+        )
+    pseudotorsions_o[0] == pseudotorsions_s[0]
 
