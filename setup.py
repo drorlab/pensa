5d1919f24dc0958d985af5118a62256a9277e500
martinvoegele <martinvoegele1989@gmail.com>
2023-10-02 01:21:26 -0400

    increase version count for next release

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 from setuptools import setup, find_packages
 
-setup(name='pensa',
-      version='0.2.8',
-      description='PENSA - protein ensemble analysis',
-      url='http://github.com/drorlab/pensa',
-      author='Martin Voegele, Neil Thomson, Sang Truong, Jasper McAvity',
-      author_email='mvoegele@stanford.edu',
-      license='MIT',
-      packages=find_packages(include=[
-        'pensa',
-        'pensa.preprocessing',
-        'pensa.features',
-        'pensa.comparison',
-        'pensa.dimensionality',
-        'pensa.clusters',
-        'pensa.statesinfo',
-      ]),
-      zip_safe=False,
-      install_requires=[
+setup(
+    name='pensa',
+    version='0.3.0',
+    description='PENSA - Protein ENSemble Analysis',
+    url='http://github.com/drorlab/pensa',
+    author='Martin Voegele, Neil Thomson, Sang Truong, Jasper McAvity',
+    author_email='martinvoegele1989@gmail.com',
+    license='MIT',
+    packages=find_packages(
+        include=[
+            'pensa',
+            'pensa.preprocessing',
+            'pensa.features',
+            'pensa.comparison',
+            'pensa.dimensionality',
+            'pensa.clusters',
+            'pensa.statesinfo',
+        ]
+    ),
+    zip_safe=False,
+    install_requires=[
         'numpy',
         'scipy>=1.2',
         'pandas',
@@ -25,9 +28,9 @@ setup(name='pensa',
         'deeptime',
         'MDAnalysis',
         'biotite',
-        'gpcrmining', 
-      ],
-      classifiers=[
+        'gpcrmining',
+    ],
+    classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
@@ -37,5 +40,5 @@ setup(name='pensa',
         'License :: OSI Approved :: MIT License',
         # Supported Python versions
         'Programming Language :: Python :: 3',
-      ],)
-
+    ],
+)
